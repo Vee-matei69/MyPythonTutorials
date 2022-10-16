74180b1d5acdc2bd19a41a4e8c3783da93d593e0
Vee-matei69 <vee.matei100@gmail.com>
2022-10-16 23:24:57 +0300

    kcpe marks entry

---
+++ day1/input_kcpe_marks.py
@@ -0,0 +1,13 @@
+#taking marks from the user
+maths = int(input('Enter your math marks: ' ))
+english = int(input('Enter your english marks: ' ))
+science = int(input('Enter your science marks: ' ))
+social_studies= int(input('Enter your social studies marks: ' ))
+kiswahili = int(input('Enter your kiswahili marks: ' ))
+
+#calculate total marks
+total_marks = maths + english + science + social_studies + kiswahili
+
+#displaying total marks
+print('Your total marks is: ', total_marks)
+
---
+++ day1/kcpe_marks.py
@@ -0,0 +1,11 @@
+maths = 80
+science = 78
+social_studies = 80
+kiswahili = 78
+english = 70
+
+#getting total marks
+total_marks = maths + science + social_studies + kiswahili + english
+
+#displaying total marks
+print('your total marks is: ', total_marks)
