0f32ac5c13160e28bd2874fac26934d1a15da0c8
Vee-matei69 <vee.matei100@gmail.com>
2022-10-23 21:57:45 +0300

    day4

---
+++ day4/kcpe_advanced.py
@@ -0,0 +1,10 @@
+students = []
+
+
+# for(int i=0; i<5; i++)
+for i in range(5):
+    name = input('Enter name of student: ')
+    students.append(name)
+
+print(students)
+
---
+++ day4/lists.py
@@ -0,0 +1,8 @@
+
+my_list = ['foo', 'bar', 'baz', 'ber']
+fruits = ['apple', 'avocado','banana', 'mango', 'guava']
+
+
+for fruit in fruits:
+    print('we are at fruit', fruit)
+
